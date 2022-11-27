62425a80cdd280f8872b0e26393e6c46fa46e7b2
OXYNTE <95431443+OXYNTE@users.noreply.github.com>
2022-11-27 18:33:41 +0100

    Update zachleat.js

--- _data/sites/zachleat.js
+++ _data/sites/zachleat.js
@@ -1,6 +1,5 @@
 module.exports = {
-	name: "zachleat.com", // optional, falls back to object key
-	description: "Zach’s Personal web site",
+	description: "Personal",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
@@ -13,17 +12,10 @@ module.exports = {
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.zachleat.com/",
-		"https://www.zachleat.com/web/",
-		"https://www.zachleat.com/web/about/",
-		"https://www.zachleat.com/web/fonts/",
-		"https://www.zachleat.com/web/eleventy/",
-		"https://www.zachleat.com/resume/",
-		"https://www.zachleat.com/twitter/",
-		// Popular Posts
-		"https://www.zachleat.com/web/lighthouse-in-footer/",
-		"https://www.zachleat.com/web/speedlify/",
-		"https://www.zachleat.com/web/comprehensive-webfonts/",
-		"https://www.zachleat.com/web/google-fonts-display/",
+		"http://localhost:8080/index.html",
+		"http://localhost:8080/curriculum/",
+		"http://localhost:8080/blog/",
+		"http://localhost:8080/colophon//",
+		"http://localhost:8080/blog/post1//",
 	]
 };
